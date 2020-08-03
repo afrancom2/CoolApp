c963350d78990c46aa11905d5ff867ce3d21fe43
afrancom2 <53576402+afrancom2@users.noreply.github.com>
2020-08-03 15:10:38 -0500

    Update data.service.ts

--- src/app/shared/data.service.ts
+++ src/app/shared/data.service.ts
@@ -7,20 +7,25 @@ export class DataService {
 
   private productos = [
     {id: 1,
-    nombre: 'huevo',
-    categoria: 'comida',
-    descripcion: 'Gran huevo',
-    precio: 2},
+    nombre: 'Huevo',
+    categoria: 'Comida',
+    descripcion: 'Se presentan protegidos por una cáscara y son ricos en proteínas y lípidos',
+    precio: 300},
     {id: 2,
     nombre: 'Coca',
     categoria: 'Perro',
     descripcion: 'Cocanavirus',
-    precio: 2},
+    precio: 150000},
     {id: 3,
-    nombre: 'Celuco',
-    categoria: 'tecnologia',
-    descripcion: 'Celular',
-    precio: 4}
+    nombre: 'Celucar',
+    categoria: 'Tecnologia',
+    descripcion: 'teléfono móvil​ es un teléfono portátil que puede hacer o recibir llamadas a través de una portadora de radiofrecuencia, mientras el usuario se está moviendo dentro de un área de servicio telefónico.',
+    precio: 700000},
+    {id: 4,
+    nombre: 'Palkel',
+    categoria: 'Pequeña',
+    descripcion: 'Nacida entle cubano',
+    precio: 1298389127389712893712}
   ];
 
   getAllProducts() {
